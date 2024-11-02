bced0e50aacd0b8896266c0a328500cba76a80a7
sagartupvihire <sagartup20@gmail.com>
2024-11-02 15:26:31 +0530

    mees

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,31 +1,31 @@
 import React, { useEffect, useRef } from 'react'
-import Message from './message.jsx'
+import Message from './message'
 import useGetMessages from '../../hooks/useGetMessages'
 import MessageSkeleton from '../skeletons/MessagesSkeletons'
 const Messages = () => {
-  const { messages, loading } = useGetMessages();
-  
-  const lastMessageRef = useRef()
-  useEffect(() => {
-    setTimeout(() => {
-      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+    const { messages, loading } = useGetMessages();
 
-    }, 1000);
-  }, [messages])
+    const lastMessageRef = useRef()
+    useEffect(() => {
+        setTimeout(() => {
+            lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
 
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-      {!loading && messages.length > 0 && messages.map((message) => (
-        <div key={message._id} ref={lastMessageRef}>
-          <Message message={message} />
+        }, 1000);
+    }, [messages])
+
+    return (
+        <div className='px-4 flex-1 overflow-auto'>
+            {!loading && messages.length > 0 && messages.map((message) => (
+                <div key={message._id} ref={lastMessageRef}>
+                    <Message message={message} />
+                </div>
+            ))}
+            {loading &&
+                [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+            {!loading && messages.length === 0 && <div className='text-center'>Send a message to start conversation</div>}
         </div>
-      ))}
-      {loading &&
-        [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-
-      {!loading && messages.length === 0 && <div className='text-center'>Send a message to start conversation</div>}
-    </div>
-  )
+    )
 }
 
 export default Messages

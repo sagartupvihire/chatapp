d51bfb4d7d282565bdade46f5e76ccbb65818499
sagartupvihire <sagartup20@gmail.com>
2024-11-02 15:42:32 +0530

    hi

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,58 +1,31 @@
-import { useEffect, useRef } from "react";
-import useGetMessages from "../../hooks/useGetMessages";
-import MessageSkeleton from "../skeletons/MessageSkeleton";
-import Message from "./Message";
-import useListenMessages from "../../hooks/useListenMessages";
-
+import { useEffect, useRef } from 'react'
+import Message from './message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessagesSkeletons'
 const Messages = () => {
-	const { messages, loading } = useGetMessages();
-	useListenMessages();
-	const lastMessageRef = useRef();
+  const { messages, loading } = useGetMessages();
+  
+  const lastMessageRef = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
 
-	useEffect(() => {
-		setTimeout(() => {
-			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
-		}, 100);
-	}, [messages]);
+    }, 1000);
+  }, [messages])
 
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			{!loading &&
-				messages.length > 0 &&
-				messages.map((message) => (
-					<div key={message._id} ref={lastMessageRef}>
-						<Message message={message} />
-					</div>
-				))}
+  return (
+    <div className='px-4 flex-1 overflow-auto'>
+      {!loading && messages.length > 0 && messages.map((message) => (
+        <div key={message._id} ref={lastMessageRef}>
+          <Message message={message} />
+        </div>
+      ))}
+      {loading &&
+        [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
-			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-			{!loading && messages.length === 0 && (
-				<p className='text-center'>Send a message to start the conversation</p>
-			)}
-		</div>
-	);
-};
-export default Messages;
+      {!loading && messages.length === 0 && <div className='text-center'>Send a message to start conversation</div>}
+    </div>
+  )
+}
 
-// STARTER CODE SNIPPET
-// import Message from "./Message";
-
-// const Messages = () => {
-// 	return (
-// 		<div className='px-4 flex-1 overflow-auto'>
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 			<Message />
-// 		</div>
-// 	);
-// };
-// export default Messages;
+export default Messages
